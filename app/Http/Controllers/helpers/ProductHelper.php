cadafe89cdf50b29bc8192ab9dec67232d772774
yi zhang <yizhang19801001@outlook.com>
2019-03-31 14:44:09 +1100

    remove debug code

--- app/Http/Controllers/helpers/ProductHelper.php
+++ app/Http/Controllers/helpers/ProductHelper.php
@@ -54,7 +54,6 @@ class ProductHelper
                 $image_path = config("app.baseurl") . $product["image"];
 
                 if ($product["image"] === null || $product["image"] === "" || !file_exists($_SERVER['DOCUMENT_ROOT'] . $image_path)) {
-                    die(json_encode($_SERVER['DOCUMENT_ROOT'] . $image_path));
                     $product["image"] = url('/') . '/images/products/default.jpeg';
                 } else {
 
